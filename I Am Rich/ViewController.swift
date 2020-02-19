e9574370dcdac4dfa8c6b82ff7ea48f99a7d74b4
Siddhika Ghaisas <siddhikapg@gmail.com>
2020-02-19 09:27:46 -0600

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Ketan Jogalekar on 2/19/20.
+//  Copyright © 2020 Siddhika Ghaisas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
